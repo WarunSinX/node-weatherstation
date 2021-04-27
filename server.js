c4ae7ce3652af80c8ae3d3e309adbd6bef2bc4eb
warunsinx <warunsinx@gmail.com>
2021-04-27 21:27:32 +0700

    add temp route

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const app = require("express")();
 const PORT = process.env.PORT || 5000;
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log("a");
   res.send("b");
+});
+
+app.post("/api/temp", (req, res) => {
+  console.log(req.query.temp);
+  res.send("Temp : " + req.query.temp);
 });
 
 app.listen(PORT, () => console.log("Server has started !"));
